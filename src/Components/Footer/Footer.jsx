5bfff82b7a13bdc3fc310dd13a5a14b9e38084e5
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2024-06-08 03:41:27 +0530

    addd

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import './Footer.scss'
 const Footer = () => {
     return (
-        <footer className="bg-radial-gradient text-gray-300 py-12">
-            <div className="container mx-auto flex flex-wrap justify-between">
+        <footer className="bg-radial-gradient text-gray-300 py-12 p-4">
+            <div className="container mx-auto flex flex-wrap justify-between text-center">
                 <div className="w-full lg:w-1/2 xl:w-1/3 mb-8 lg:mb-0">
-                    <h2 className="text-xl font-semibold mb-4">About Us</h2>
+                    <h2 className="text-4xl font-semibold mb-4">About Me</h2>
                     <p>We are a team of passionate developers dedicated to creating innovative solutions.</p>
                 </div>
                 <div className="w-full lg:w-1/2 xl:w-1/3 mb-8 lg:mb-0">
@@ -14,22 +13,11 @@ const Footer = () => {
                     <p>Phone: +1234567890</p>
                 </div>
                 <div className="w-full lg:w-1/2 xl:w-1/3">
-                    <h2 className="text-xl font-semibold mb-4">Follow Us</h2>
-                    <div className="flex space-x-4">
-                        <a href="#" className="text-gray-300 hover:text-gray-400 transition-colors duration-300">
-                            <i className="fab fa-facebook-f"></i>
-                        </a>
-                        <a href="#" className="text-gray-300 hover:text-gray-400 transition-colors duration-300">
-                            <i className="fab fa-twitter"></i>
-                        </a>
-                        <a href="#" className="text-gray-300 hover:text-gray-400 transition-colors duration-300">
-                            <i className="fab fa-instagram"></i>
-                        </a>
-                    </div>
                 </div>
             </div>
+            
             <div className="text-center mt-8">
-                <p className="text-sm">&copy; 2024 Your Website. All rights reserved.</p>
+                <p className="text-sm">@DhirajBhawsar 2024 rights reserved.</p>
                 <p className="mt-2">Thanks for visiting!</p>
             </div>
         </footer>
--- src/Components/ProjectSection/ProjecSection.jsx
+++ src/Components/ProjectSection/ProjecSection.jsx
@@ -1,98 +1,142 @@
-import { useEffect, useState } from 'react';
-import { motion } from 'framer-motion';
-import ProjectOneImage from './../../assets/Img/aaaaaaaaaaaa.jpg';
-import ProjectTwoImage from './../../assets/Img/aaaaaaaaaaaa.jpg';
-import ProjectThreeImage from './../../assets/Img/aaaaaaaaaaaa.jpg';
-import "./ProjectSection.scss"
+import { motion } from "framer-motion";
+
+
+
+
+import ProjectOneImage from "./../../assets/Img/Gmail Project.png";
+import ProjectTwoImage from "./../../assets/Img//gym.png";
+import ProjectThreeImage from "./../../assets/Img/phone.png";
+import ProjectFourImage from "./../../assets/Img/AgeCal.png";
+import ProjectSixImage from "./../../assets/Img/PassGen.png";
+import ProjectFiveImage from "./../../assets/Img/emoji.png";
+import ProjectSevenImage from "./../../assets/Img/githubFinder.png"
+import "./ProjectSection.scss";
+
+// Material ui
+import GitHubIcon from "@mui/icons-material/GitHub";
+import AttachmentIcon from '@mui/icons-material/Attachment';
 
 const ProjectSection = () => {
-    const [isVisible, setIsVisible] = useState(false);
+  const projects = [
+    {
+      title: "Gmail Clone",
+      description:
+        "This Gmail clone project demonstrates my ability to develop a web application with modern technologies, focusing on user authentication and email functionality. It showcases my skills in front-end development with HTML, CSS, and JavaScript, as well as my ability to integrate third-party APIs for extended functionality.",
+      link: "https://github.com/Dhirajbhavsar9900/Geekthone_GmailClone  ",
+      image: ProjectOneImage,
+      goLive: "https://geekthone-gmail-clone.vercel.app/",
+    },
+    {
+        title: "GitHub Finder",
+        description:
+          "This Gmail clone project demonstrates my ability to develop a web application with modern technologies, focusing on user authentication and email functionality. It showcases my skills in front-end development with HTML, CSS, and JavaScript, as well as my ability to integrate third-party APIs for extended functionality.",
+        link: "https://github.com/Dhirajbhavsar9900/Geekthone_GmailClone  ",
+        image: ProjectSevenImage,
+        goLive: "https://geekthone-gmail-clone.vercel.app/",
+      },
+    {
+      title: "Gym Website ",
+      description:
+        "GymFit is a modern, responsive web application designed to provide users with a comprehensive fitness experience. The website showcases a variety of exercises through engaging GIFs, helping users to understand and perform exercises correctly.",
+      link: "https://github.com/Dhirajbhavsar9900/GYMFIT-REACT",
+      image: ProjectTwoImage,
+      goLive: "https://gymfit-opal.vercel.app/  ",
+    },
+    {
+      title: "Phone Search",
+      description:
+        "I developed a fully responsive mobile search website using HTML, CSS (Tailwind CSS), and JavaScript. This project demonstrates my skills in front-end web development and my ability to create dynamic, user-friendly interfaces. The website is designed to search for information on a particular website via an API, providing real-time results.",
+      link: "https://github.com/Dhirajbhavsar9900/Iphone-web-Weakly-test-4-",
+      image: ProjectThreeImage,
+      goLive: "https://iphone-web-weakly-test-4.vercel.app/",
+    },
+    {
+      title: "Age Calculator",
+      description:
+        "I developed a user-friendly Age Calculator using HTML, CSS, Tailwind, and JavaScript. This project demonstrates my skills in front-end development, including responsive design and interactive user interfaces. The Age Calculator allows users to input their birthdate and quickly find out their exact age in years, months, and days.",
+      link: "https://github.com/Dhirajbhavsar9900/Age_Calculator",
+      image: ProjectFourImage,
+      goLive: "https://dhirajbhavsar9900.github.io/Age_Calculator/",
+    },
+    {
+      title: "Emoji Search",
+      description:
+        "The Emoji Search Website is an interactive web application designed to help users find and copy their favorite emojis quickly and efficiently. This project was built using HTML, CSS, and JavaScript, with a focus on providing a seamless user experience through intuitive design and functionality.",
+      link: "https://github.com/Dhirajbhavsar9900/Emojis_Searching_Project",
+      image: ProjectFiveImage,
+      goLive: "https://emojis-searching-project.vercel.app/",
+    },
+    {
+      title: "PassWord Generator",
+      description:
+        "I developed a Password Generator website using React and Vite, aiming to provide users with a secure and convenient way to generate strong, random passwords. This project highlights my skills in modern frontend development, including state management, user interface design, and performance optimization with Vite.",
+      link: "https://weakly-test-01.vercel.app/",
+      image: ProjectSixImage,
+      goLive: "https://github.com/Dhirajbhavsar9900/Weakly-Test-01",
+    },
+  ];
+  
 
-    useEffect(() => {
-        const handleScroll = () => {
-            const projectSection = document.getElementById('project-section');
-            const offsetTop = projectSection.offsetTop;
-            const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
-
-            if (scrollTop > offsetTop - window.innerHeight / 2) {
-                setIsVisible(true);
-            }
-        };
-
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    const projects = [
-        {
-            title: "Project One",
-            description: "This is a brief description of Project One. It involves using technology X and Y to achieve Z.",
-            link: "https://github.com/yourusername/project-one",
-            image: ProjectOneImage,
-            goLive : "https://github.com/yourusername/project-one"
-        },
-        {
-            title: "Project Two",
-            description: "This is a brief description of Project Two. It focuses on solving problem A using tool B.",
-            link: "https://github.com/yourusername/project-two",
-            image: ProjectTwoImage,
-            goLive : "https://github.com/yourusername/project-one"
-        },
-        {
-            title: "Project Three",
-            description: "This is a brief description of Project Three. It is a collaborative project that implements feature C.",
-            link: "https://github.com/yourusername/project-three",
-            image: ProjectThreeImage,
-            goLive : "https://github.com/yourusername/project-one"
-        }
-    ];
-
-    return (
-        <div id="project-section" className=" py-24 px-5 text-center bg-slate-300 from-gray-200 to-gray-300">
-            <h1 className="text-black drop-shadow-lg text-[50px]  mb-10 mt-10 font-bold">Projects</h1>
-            <div className="flex flex-wrap justify-center">
-                {projects.map((project, index) => (
-                    <motion.div
-                        key={index}
-                        className="bg-white border cursor-pointer border-gray-300 rounded-lg m-4 p-5 w-80 shadow-md transition-transform duration-300 hover:translate-y-[-10px] hover:shadow-lg"
-                        initial={{ opacity: 0, y: 20 }}
-                        animate={isVisible ? { opacity: 1, y: 0 } : {}}
-                        transition={{ duration: 0.5, delay: index * 0.2 }}
-                        whileHover={{ scale: 1.05 }}
+  return (
+    <div
+      id="project-section"
+      className="py-24 px-5 text-center bg-slate-300 from-gray-200 to-gray-300"
+    >
+      <h2 className="text-5xl mb-14 font-bold">Projects</h2>
+      <div className="flex flex-wrap justify-center">
+        {projects.map((project, index) => (
+            <motion.div
+                key={index}
+                className="relative m-3 cursor-pointer flex flex-col text-gray-700 bg-black shadow-md bg-clip-border rounded-xl w-96"
+                initial={{ opacity: 0, y: 50 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.6, delay: index * 0.1 }} // Delay added for stagger effect
+                whileHover={{ scale: 1.03, boxShadow: "0px 10px 30px rgba(0, 0, 0, 0.2)" }} // Scale up and add shadow on hover
+                whileTap={{ scale: 0.97 }} // Scale down slightly on tap
+            >
+                <motion.div
+                className="relative h-56 mx-4 mt-5 overflow-hidden rounded-xl bg-blue-gray-500 shadow-blue-gray-500/40"
+                whileHover={{ opacity: 0.9 }} // Reduce opacity on hover
+                >
+                <img src={project.image} alt="card-image" className="w-full h-full object-cover rounded-xl" />
+                <motion.div
+                    className="absolute inset-0 flex justify-center items-center opacity-0 transition-opacity duration-300"
+                    whileHover={{ opacity: 1 }} 
+                >
+                    <div className="absolute inset-0 bg-black opacity-70 rounded-xl "></div>
+                    <div className="flex gap-12 justify-center mx-2 my-2 space-x-2 z-10">
+                    <motion.a
+                        href={project.link}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        whileHover={{ scale: 1.1 }}
                     >
-                        <motion.img
-                            src={project.image}
-                            alt={project.title}
-                            className="w-full mb-5 rounded-md"
-                            whileHover={{ scale: 1.1, transition: { duration: 0.3 } }}
-                        />
-                        <h2 className="text-xl mb-4 text-blue-500">{project.title}</h2>
-                        <p className="text-gray-600 mb-5">{project.description}</p>
-                        <div className="flex justify-center items-center space-x-4">
-                            <a href={project.link} target="_blank" rel="noopener noreferrer" className="font-bold transition-colors duration-300">
-                                <motion.button
-                                    className="bg-blue-500 text-white px-6 py-3 rounded-xl m-1 shadow-lg hover:bg-blue-600 hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
-                                    whileHover={{ scale: 1.1 }}
-                                >
-                                    GitHub
-                                </motion.button>
-                            </a>
-                            <a href={project.link} target="_blank" rel="noopener noreferrer" className="font-bold transition-colors duration-300">
-                                <motion.button
-                                    className="bg-red-500 text-white px-6 py-3 rounded-xl m-1 shadow-lg hover:bg-red-600 hover:shadow-xl transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-105"
-                                    whileHover={{ scale: 1.1 }}
-                                >
-                                    Go Live
-                                </motion.button>
-                            </a>
-                        </div>
-                    </motion.div>
-                ))}
-            </div>
-        </div>
-    );
-}
+                        <GitHubIcon style={{ fontSize: "45px", fill :"#FFD700",}} />
+                    </motion.a>
+                    <motion.a
+                        href={project.goLive}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        whileHover={{ scale: 1.1 }}
+                    >
+                        <AttachmentIcon style={{ fontSize: "45px", fill: "#FBF9EF" }} />
+                    </motion.a>
+                    </div>
+                </motion.div>
+                </motion.div>
+                <div className="p-6">
+                <h5 className="block my-3 font-Josefin text-2xl font-semibold leading-snug text-white">
+                    {project.title}
+                </h5>
+                <p className="block font-sans text-base text-gray-300 leading-relaxed">
+                    {project.description}
+                </p>
+                </div>
+            </motion.div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default ProjectSection;
