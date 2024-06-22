f3e60a24f0c49219a529d3af674d87a6fbf7a5ea
Dhiraj Bhawsar <dhirajbhavsar.official@gmail.com>
2024-06-22 20:10:26 +0530

    change hero section

--- src/Components/HeroSection/HeroSection.jsx
+++ src/Components/HeroSection/HeroSection.jsx
@@ -134,7 +134,7 @@ const HeroSection = () => {
             onClick={handleContactInfo}
           >
             <div id="btn" className="flex justify-center items-center">
-              <span className="p-2 fill-white">Contact Info</span>
+              <span className="p-2 fill-white">Contact</span>
               <svg
                 className="m-2 w-5 fill-white"
                 xmlns="http://www.w3.org/2000/svg"
